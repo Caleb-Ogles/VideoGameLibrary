5576b37949f7557f2831ab623298252425352df7
Caleb Ogles <calebogles@pc_18857.barren.ketsds.net>
2018-09-10 12:44:42 -0500

    Created repository for VideoGameLibrary

---
+++ VideoGameLibrary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VideoGameLibrary
+//
+//  Created by Caleb Ogles on 9/10/18.
+//  Copyright © 2018 Caleb Ogles. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
